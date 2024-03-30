8c55fbcf730886003daed164c6a1286853bb5d5f
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-30 09:59:47 -0700

    failing

---
+++ script01.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# remove container ubc if it exists
+lxc ls --format=json | jq 'map(select(.name == "ubc")) | .[] | .name' | xargs --no-run-if-empty -I {} lxc delete --force {}
+
+# launch ubuntu 22.04 container and name it ubc
+lxc launch ubuntu:22.04 ubc
+lxc ls
--- src/itfruitbeer/__init__.py
+++ src/itfruitbeer/__init__.py
@@ -9,9 +9,27 @@ __project_name__ = "itfruitbeer"
 
 def main() -> int:
     out = main2.render_template("readme/readme.md.j2")
-    readme = pathlib.Path("README.md")
-    readme.write_text(out)
-    toc = md_toc.build_toc(readme)
-    md_toc.write_string_on_file_between_markers(readme, toc, "<!--TOC-->")
+    outfile = pathlib.Path("README.md")
+    outfile.write_text(out)
+    toc = md_toc.build_toc(outfile)
+    md_toc.write_string_on_file_between_markers(outfile, toc, "<!--TOC-->")
+
+    scripts = {
+        "readme/script01.sh.j2": {
+            "outfile": "script01.sh",
+        },
+        "readme/script02.sh.j2": {
+            "outfile": "script02.sh",
+        },
+        "readme/script03.sh.j2": {
+            "outfile": "script03.sh",
+        },
+    }
+
+    for script, config in scripts.items():
+        out = main2.render_template(script)
+        outfile = pathlib.Path(config["outfile"])
+        outfile.write_text(out)
+        outfile.chmod(0o775)
 
     return 0
