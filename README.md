910835e855255e0f9dee5f885dd639169a791573
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-30 14:37:15 -0700

    ok

--- README.md
+++ README.md
@@ -25,12 +25,6 @@ apt-get --assume-yes install lxc lxc-utils jq
 lxd init --auto
 lxc ls
 
-# remove container ubc if it exists
-lxc ls --format=json | jq 'map(select(.name == "ubc")) | .[] | .name' | xargs --no-run-if-empty -I {} lxc delete --force {}
-
-# launch ubuntu 22.04 container and name it ubc
-lxc launch ubuntu:22.04 ubc
-lxc ls
 ```
 
 
--- src/itfruitbeer/__init__.py
+++ src/itfruitbeer/__init__.py
@@ -8,10 +8,26 @@ __project_name__ = "itfruitbeer"
 
 
 def main() -> int:
-    out = main2.render_template("readme/readme.md.j2")
-    readme = pathlib.Path("README.md")
-    readme.write_text(out)
-    toc = md_toc.build_toc(readme)
-    md_toc.write_string_on_file_between_markers(readme, toc, "<!--TOC-->")
+    rendered = main2.render_template("readme/readme.md.j2")
+    outfile = pathlib.Path("README.md")
+    outfile.write_text(rendered)
+    toc = md_toc.build_toc(outfile)
+    md_toc.write_string_on_file_between_markers(outfile, toc, "<!--TOC-->")
+
+    scripts = [
+        {
+            "outfile": "script01.sh",
+            "tmpl": "readme/script01.sh.j2",
+        }
+    ]
+
+    for dct in scripts:
+        rendered = main2.render_template(dct["tmpl"])
+        outfile = pathlib.Path(dct["outfile"])
+        outfile.write_text(rendered)
+        pathlib.Path(outfile)
+
+        if outfile.suffix == ".sh":
+            outfile.chmod(0o755)
 
     return 0
