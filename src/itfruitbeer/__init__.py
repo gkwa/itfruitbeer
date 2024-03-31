5ae963b3f31c9f3a9783f72c4fa5758fad89f98c
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-30 22:34:09 -0700

    reorg

--- src/itfruitbeer/__init__.py
+++ src/itfruitbeer/__init__.py
@@ -1,3 +1,4 @@
+import dataclasses
 import pathlib
 
 import md_toc
@@ -5,6 +6,12 @@ import md_toc
 from . import main2
 
 __project_name__ = "itfruitbeer"
+
+
+@dataclasses.dataclass
+class Script:
+    outfile: str
+    tmpl: str
 
 
 def main() -> int:
@@ -15,25 +22,15 @@ def main() -> int:
     md_toc.write_string_on_file_between_markers(outfile, toc, "<!--TOC-->")
 
     scripts = [
-        {
-            "outfile": "script01.sh",
-            "tmpl": "readme/script01/script.sh.j2",
-        },
-        {
-            "outfile": "script02.sh",
-            "tmpl": "readme/script02/script.sh.j2",
-        },
-        {
-            "outfile": "script03.sh",
-            "tmpl": "readme/script03/script.sh.j2",
-        },
+        Script(outfile="script01.sh", tmpl="readme/script01/script.sh.j2"),
+        Script(outfile="script02.sh", tmpl="readme/script02/script.sh.j2"),
+        Script(outfile="script03.sh", tmpl="readme/script03/script.sh.j2"),
     ]
 
-    for dct in scripts:
-        rendered = main2.render_template(dct["tmpl"])
-        outfile = pathlib.Path(dct["outfile"])
+    for script in scripts:
+        rendered = main2.render_template(script.tmpl)
+        outfile = pathlib.Path(script.outfile)
         outfile.write_text(rendered)
-
         if outfile.suffix == ".sh":
             outfile.chmod(0o755)
 
